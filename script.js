63216098b28cb874c04771a13bb3f6973a509822
rafael <rafadoria1@gmail.com>
2022-01-18 16:38:35 +0900

    Added basic game

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function computerPlay() {
         return "Scissors"
     }
 }
+
+function playRound(player, computer) {
+    player = player.toLowerCase();
+    if (player === computer) {
+        return "It's a Tie!"
+    } 
+    else if (player === "rock") {
+        if (computer === "paper") {
+            return "You Lose! Paper beats Rock!"
+        }
+        else return "You Win! Rock beats Scissors!"
+    }
+    else if (player === "paper") {
+        if (computer === "rock") {
+            return "You win! Paper beats Rock!"
+        }
+        else return "You Lose! Scissors beat Paper!"
+    }
+    else {
+        if (computer === "rock") {
+            return "You Lose! Rock beat Scissors"
+        }
+        else return "You Win! Scissors beat Paper!"
+    }
+}
+
+console.log(playRound("scissors" ,"paper"));
