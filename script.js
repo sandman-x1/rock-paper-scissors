68fd1bf5369aba97794872a2939ef88bd3ab2ce7
rafael <rafadoria1@gmail.com>
2022-01-18 18:55:01 +0900

    Added to console message for results

--- script.js
+++ script.js
@@ -14,36 +14,50 @@ function playRound(player, computer) {
     player = player.toLowerCase();
     if (player === computer) {
         tieCounter++
-        return "It's a Tie!"
+        console.log("You chose " + player + "\n" +
+                    "The computer chose " + computer + "\n" +
+                    "It's a Tie!");
     } 
     else if (player === "rock") {
         if (computer === "paper") {
             computerWins++
-            return "You Lose! Paper beats Rock!"
+            console.log("You chose " + player + "\n" +
+                        "The computer chose " + computer + "\n" +
+                        "You Lose! Paper beats Rock!");
         }
         else {
             playerWins++
-            return "You Win! Rock beats Scissors!"
+            console.log("You chose " + player + "\n" +
+                        "The computer chose " + computer + "\n" +
+                        "You Win! Rock beats Scissors!");
         }
     }
     else if (player === "paper") {
         if (computer === "rock") {
             playerWins++
-            return "You win! Paper beats Rock!"
+            console.log("You chose " + player + "\n" +
+                        "The computer chose " + computer + "\n" +
+                        "You win! Paper beats Rock!");
         }
         else {
             computerWins++
-            return "You Lose! Scissors beat Paper!"
+            console.log("You chose " + player + "\n" +
+                        "The computer chose " + computer + "\n" +
+                        "You Lose! Scissors beat Paper!");
         }
     }
     else {
         if (computer === "rock") {
             computerWins++
-            return "You Lose! Rock beat Scissors"
+            console.log("You chose " + player + "\n" +
+                        "The computer chose " + computer + "\n" +
+                        "You Lose! Rock beat Scissors");
         }
         else {
             playerWins++
-            return "You Win! Scissors beat Paper!"
+            console.log("You chose " + player + "\n" +
+                        "The computer chose " + computer + "\n" +
+                        "You Win! Scissors beat Paper!");
         }
     }
 }
@@ -56,4 +70,7 @@ function game() {
         let choice = prompt("Please choose between Rock, Paper or Scissors");
         playRound(choice, computerPlay());
     }
+    
 }
+
+game();
