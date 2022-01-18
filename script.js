f5b3a56f0f5b846441b5e54a1bc0922c548d192a
rafael <rafadoria1@gmail.com>
2022-01-18 21:53:02 +0900

    Simplified console.log dialogs

--- script.js
+++ script.js
@@ -14,50 +14,36 @@ function playRound(player, computer) {
     player = player.toLowerCase();
     if (player === computer) {
         tieCounter++
-        console.log("You chose " + player + "\n" +
-                    "The computer chose " + computer + "\n" +
-                    "It's a Tie!");
+        console.log(`You chose ${player}\nThe computer chose ${computer}\nIt's a Tie!`);
     } 
     else if (player === "rock") {
         if (computer === "paper") {
             computerWins++
-            console.log("You chose " + player + "\n" +
-                        "The computer chose " + computer + "\n" +
-                        "You Lose! Paper beats Rock!");
+            console.log(`You chose ${player}\nThe computer chose ${computer}\nYou Lose! Paper beats Rock!`);
         }
         else {
             playerWins++
-            console.log("You chose " + player + "\n" +
-                        "The computer chose " + computer + "\n" +
-                        "You Win! Rock beats Scissors!");
+            console.log(`You chose ${player}\nThe computer chose ${computer}\nYou Win! Rock beats Scissors!`);
         }
     }
     else if (player === "paper") {
         if (computer === "rock") {
             playerWins++
-            console.log("You chose " + player + "\n" +
-                        "The computer chose " + computer + "\n" +
-                        "You win! Paper beats Rock!");
+            console.log(`You chose ${player}\nThe computer chose ${computer}\nYou win! Paper beats Rock!`);
         }
         else {
             computerWins++
-            console.log("You chose " + player + "\n" +
-                        "The computer chose " + computer + "\n" +
-                        "You Lose! Scissors beat Paper!");
+            console.log(`You chose ${player}\nThe computer chose ${computer}\nYou Lose! Scissors beat Paper!`);
         }
     }
     else {
         if (computer === "rock") {
             computerWins++
-            console.log("You chose " + player + "\n" +
-                        "The computer chose " + computer + "\n" +
-                        "You Lose! Rock beat Scissors");
+            console.log(`You chose ${player}\nThe computer chose ${computer}\nYou Lose! Rock beat Scissors`);
         }
         else {
             playerWins++
-            console.log("You chose " + player + "\n" +
-                        "The computer chose " + computer + "\n" +
-                        "You Win! Scissors beat Paper!");
+            console.log(`You chose ${player}\nThe computer chose ${computer}\nYou Win! Scissors beat Paper!`);
         }
     }
 }
